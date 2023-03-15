fb392bd122c0a290b184552acc0f341177dcd709
thibault-barrat <contact@thibault-barrat.com>
2023-03-15 17:37:50 +0100

    Fix: publish script

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -1,17 +1,6 @@
-TAG=${GITHUB_REF##*/}
-echo "TAG"
-echo $TAG
-BRANCHES=$(git branch -r --contains tags/$TAG)
-echo "BRANCHES"
-echo $BRANCHES
-for BRANCH in $BRANCHES; do
-    echo "BRANCH"
-    echo $BRANCH
-#   if [[ $branch == "origin/main" ]]; then
-#     onProtectedBranch=true
-#   fi
-done
-if [ "$TAG" != "" ] && [ "$BRANCH" = "origin/main" ]
+HEAD_TAG=$(git tag --points-at HEAD)
+REF=${GITHUB_REF##*/}
+if [ "$HEAD_TAG" != "" ] && [ "$REF" = "main" ]
 then 
     echo "Publishing"
     dir_path=$(dirname $(realpath $0))
